5bda53e29f540d589f5c9119414da85e328bb38c
noraarellano <72581638+noraarellano@users.noreply.github.com>
2023-12-01 16:50:33 -0600

    finished

---
+++ src/app/vincular/vincular.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-vincular',
+  standalone: true,
+  imports: [],
+  templateUrl: './vincular.component.html',
+  styleUrl: './vincular.component.css'
+})
+export class VincularComponent {
+  mensaje = "Este es el mensaje original"
+  cambiarMensaje(){
+    this.mensaje="Mensaje nuevo respondiendo al click"
+  }
+}
